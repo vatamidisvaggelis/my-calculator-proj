57c1c770b5a6ee6fbbe941995aa12e4f0ab43cc7
Evangelos. Vatamidis <vatamidis_vaggelis@yahoo.gr>
2024-12-17 12:02:00 +0200

    println added

--- src/gr/aueb/cf/ch1/AddCalculator.java
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -9,6 +9,8 @@ public class AddCalculator {
         int result = 0;
 
         result = add(num1, num2);
+
+        System.out.println("Sum" + result);
     }
 
     public static int add(int a, int b){
