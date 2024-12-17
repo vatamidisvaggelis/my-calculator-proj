f5973f5be2d259fa88ce1479ed47e8d674d07191
Evangelos. Vatamidis <vatamidis_vaggelis@yahoo.gr>
2024-12-17 12:04:25 +0200

    Multiply calculator created

---
+++ src/gr/aueb/cf/ch3/MulCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch3;
+
+public class MulCalculator {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = 0;
+
+        result = mul(num1,num2);
+    }
+    public static int mul(int a, int b){
+        return a*b;
+    }
+}
